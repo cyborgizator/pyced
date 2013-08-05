ba1f3301038f1b325c24a4f0e0a6f8863d40c061
Alexey Bright <cyborgizator@gmail.com>
2013-08-05 19:52:48 +0300

    Props added

---
+++ pyced/props.py
@@ -0,0 +1,32 @@
+'''
+Various sets of data
+
+Created on 05.08.2013
+@author: Alexey Bright
+'''
+
+non_metals = {'H',
+              'He',
+              'B',
+              'C',
+              'N',
+              'O',
+              'F',
+              'Ne',
+              'Si',
+              'P',
+              'S',
+              'Cl',
+              'Ar',
+              'Ge',
+              'As',
+              'Se',
+              'Br',
+              'Kr',
+              'Te',
+              'I',
+              'Xe',
+              'At',
+              'Rn'
+              }
+
