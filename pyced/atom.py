8aa606702056d32ec14e192e0248c38c6fea0734
Alexey Bright <cyborgizator@gmail.com>
2014-02-01 20:31:38 +0200

    Added atom value calculation

--- pyced/atom.py
+++ pyced/atom.py
@@ -5,6 +5,8 @@ Created on 09.08.2013
 @author: Alexey Bright
 '''
 
+from element import E
+
 class Atom(object):
     ' Represents an atom '
     
@@ -12,20 +14,36 @@ class Atom(object):
                  element,
                  labelled = False,
                  ring = False,
-                 index = 0):
+                 index = 0,
+                 color = -1):
         ' Constructs an atom '
         self.element = element
         self.labelled = labelled
         self.ring = ring
         self.index = index
+        if color == -1:
+            self.calculate_color()
+        
+    # ----------------------------------------------------------------------- #
         
     def copy(self):
         atom = Atom(self.element,
                     self.labelled,
                     self.ring,
-                    self.index)
+                    self.index,
+                    self.color)
         return atom
-        
-    # ---------------------------------------------------------------------- #
     
+    # ----------------------------------------------------------------------- #
     
+    def calculate_color(self):
+        if self.element == E.H:
+            self.value = 0
+        else:
+            self.value = (1 << 10) if self.element == E.C else 0
+            self.value |= (ord('Z') - ord(self.element.symbol[0])) << 5
+            if len(self.element.symbol) > 1:
+                self.value |= ord('z') - ord(self.element.symbol[1])
+
+
+            
