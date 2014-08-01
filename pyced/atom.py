81b86fcbdaae79cb58a516124c8512a867070400
cyborgizator <cyborgizator@gmail.com>
2014-08-01 16:13:52 +0300

    Removed value calculation from Atom

--- pyced/atom.py
+++ pyced/atom.py
@@ -17,14 +17,3 @@ class Atom(GenericVertex):
     def copy(self):
         """ Returns copy of the atom """
         return Atom(self.__element)
-
-    # -------------------------------------------------------------------------
-    def calculate_value(self):
-        """ Calculates and returns comparison value of the atom """
-        value = 0
-        if self.__element != E.H:
-            value = (1 << 10) if self.__element == E.C else 0
-            value |= (ord('Z') - ord(self.__element.symbol[0])) << 5
-            if len(self.__element.symbol) > 1:
-                value |= ord('z') - ord(self.__element.symbol[1])
-        return value
